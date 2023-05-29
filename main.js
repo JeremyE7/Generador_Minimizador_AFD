6cc785e1e3ca0e6798ea017db70c0ab319262602
Jeremy Encalada <encaladajeremy@gmail.com>
2023-05-29 13:15:20 -0500

    Nodos Iniciales, finales, eliminar nodos y  verif

--- main.js
+++ main.js
@@ -2,25 +2,34 @@
 const canvas = new joint.dia.Paper({
   el: $('#canvas'),
   width: 800,
-  height: 600,
+  height: 400,
   gridSize: 10,
   model: new joint.dia.Graph()
 });
 
 // Función para crear un nuevo estado
 function createNode(x, y, label) {
+  if(nodes.some(n => n.label == label)){
+    alert("Ya existe un estado con ese nombre");
+    return;
+  };
   return new joint.shapes.standard.Circle({
     position: { x, y },
     size: { width: 60, height: 60 },
     attrs: {
       circle: { fill: '#3498db' },
-      text: { text: label, fill: 'white' }
+      text: { text: label, fill: 'white' },
+      body: {  fill: '#FFFFFF', stroke: '#000000', strokeWidth: 2 }
     }
   });
 }
 
 // Función para crear una nueva transición
 function createLink(source, target, label) {
+  if(links.some(l => l.source == source && l.target == target && l.label == label)){
+    alert("Ya existe una transición con esos valores");
+    return;
+  }
   const link = new joint.shapes.standard.Link({
     source: { id: source },
     target: { id: target },
@@ -40,37 +49,49 @@ function createLink(source, target, label) {
 }
 
 // Crear nodos iniciales
-const node1 = createNode(100, 100, 'A');
-const node2 = createNode(300, 150, 'B');
 
-const nodes = [node1, node2];
 
+const nodesView = [];
+const nodes = []
 // Crear transiciones iniciales
-const link1 = createLink(node1.id, node2.id, '0');
-const link2 = createLink(node2.id, node1.id, '1');
-const links = [link1, link2];
-// Añadir nodos y transiciones al canvas
-canvas.model.addCell([node1, node2, link1, link2]);
 
-// Evento de doble clic para agregar un nuevo nodo
+const linksView = [];
+const links = []
+// Añadir nodos y transiciones al canvas
+
+// Evento de clic para agregar un nuevo nodo
 canvas.on('blank:pointerclick', function(evt, x, y) {
   const label = prompt('Ingrese el nombre del nuevo estado:');
   if (label) {
     const newNode = createNode(x, y, label);
+    if(newNode == null) return;
+    nodes.push({label: label, estado: "normal"});
     canvas.model.addCell(newNode);
-    nodes.push(newNode);
+    nodesView.push(newNode);
+    console.log(nodes);
   }
 });
 
 
 // Evento de doble clic en un nodo para agregar una nueva transición
-canvas.on('element:pointerclick', function(cellView) {
+canvas.on('element:pointerclick', function(cellView, evt) {
+  if(evt.shiftKey || evt.ctrlKey) return;
   const sourceNode = cellView.model;
   const label = prompt('Ingrese el valor de la transición:');
+
+  const addLink = (source, target, label) =>{
+      source = nodesView.find(n =>  source === n.attributes.attrs.text.text);
+      target = nodesView.find(n =>  target === n.attributes.attrs.text.text);
+      links.push({source: source.attributes.attrs.text.text, target: target.attributes.attrs.text.text, label: label});
+  }
+
   if (label) {
     let targetNode = prompt('Ingrese el nombre del estado destino:')
-    targetNode = nodes.find(n =>  targetNode === n.attributes.attrs.text.text); 
-    if(targetNode == null) return;
+    targetNode = nodesView.find(n =>  targetNode === n.attributes.attrs.text.text); 
+    if(targetNode == null) {
+      alert("No existe un estado con ese nombre");
+      return;
+    };
     
     // Buscar enlace existente entre los mismos nodos de origen y destino
     const existingLink = canvas.model.getConnectedLinks(sourceNode, { outbound: true })
@@ -84,9 +105,85 @@ canvas.on('element:pointerclick', function(cellView) {
   } else {
     // Crear un nuevo enlace
     const newLink = createLink(sourceNode.id, targetNode.id, label);
-    links.push(newLink);
+    linksView.push(newLink);
     canvas.model.addCell([targetNode, newLink]);
   }
+  addLink(sourceNode.attributes.attrs.text.text, targetNode.attributes.attrs.text.text, label);
+  console.log(links);
 }
 });
 
+//Evento de clic derecho en un enlace para eliminarlo
+canvas.on('link:contextmenu', function(cellView, evt) {
+  const link = cellView.model;
+  const sourceNode = link.getSourceCell();
+  const targetNode = link.getTargetCell();
+  const labels = link.prop('labels');
+  const label = labels.length > 0 ? labels[0].attrs.text.text : '';
+  const confirm = window.confirm(`¿Está seguro de eliminar la transición ${label} de ${sourceNode.attributes.attrs.text.text} a ${targetNode.attributes.attrs.text.text}?`);
+  if (confirm) {
+    canvas.model.removeCells([link]);
+    links.splice(linksView.indexOf(link), 1);
+    linksView.splice(linksView.indexOf(link), 1);
+    
+  }
+  console.log(links);
+});
+
+//Evento de doble clic derecho en un nodo para eliminarlo
+canvas.on('element:contextmenu', function(cellView, evt) {
+  const node = cellView.model;
+  const confirm = window.confirm(`¿Está seguro de eliminar el estado ${node.attributes.attrs.text.text}?`);
+  if (confirm) {
+    canvas.model.removeCells([node]);
+    nodes.splice(nodesView.indexOf(node), 1);
+    nodesView.splice(nodesView.indexOf(node), 1);
+    
+  }
+  console.log(nodes);
+});
+
+//Evento de clik izquierdo mas shift para volverlo un estado inicial
+canvas.on('element:pointerclick', function(cellView, evt) {
+  
+  const node = cellView.model;
+  if (evt.shiftKey) {
+    if(nodes.some(n => n.estado == "inicial")) {
+      alert("Ya existe un estado inicial");
+      return;
+    };
+    if(nodes[nodesView.indexOf(node)].estado == "inicial"){
+      node.attr('body/stroke', '#000000');
+      node.attr('body/strokeWidth', 2);
+      nodes[nodesView.indexOf(node)].estado = "normal";
+      return;
+    }
+
+    const confirm = window.confirm(`¿Está seguro de volver el estado ${node.attributes.attrs.text.text} inicial?`);
+    if (confirm) {
+      node.attr('body/stroke', '#3498db');
+      node.attr('body/strokeWidth', 4);
+      nodes[nodesView.indexOf(node)].estado = "inicial";
+    }
+    console.log(nodes);
+  }
+})
+
+//Evento de clik izquierdo mas Ctrl para volverlo un estado final
+canvas.on('element:pointerclick', function(cellView, evt) {
+  const node = cellView.model;
+  if (evt.ctrlKey) {
+    if(nodes[nodesView.indexOf(node)].estado == "final"){
+      node.attr('body/stroke', '#000000');
+      node.attr('body/strokeWidth', 2);
+      nodes[nodesView.indexOf(node)].estado = "normal";
+      return;
+    }
+    const confirm = window.confirm(`¿Está seguro de volver el estado ${node.attributes.attrs.text.text} final?`);
+    if (confirm) {
+      node.attr('body/stroke', '#f2ff00');
+      node.attr('body/strokeWidth', 7);
+      nodes[nodesView.indexOf(node)].estado = "final";
+    }
+  }
+});
